ae5662ebd0c5ab44442d4d03ee5490a73831affd
PawelDuplaga <pawelduplagait@gmail.com>
2023-11-02 06:11:13 +0100

    quick improvement

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -8,9 +8,9 @@ import Slide from '../Slide';
 type slidesState = "left" | "front" | "right"
 
 const variants = {
-  left: { opacity: 0.2, scale: 0.8, zIndex: 1 },
-  front : {opacity: 1, scale: 1, x: 0, zIndex: 2 },
-  right : {opacity: 1, scale: 1, x: 400, zIndex: 3 }
+  left: { opacity: 0.2, scale: 0.8, zIndex: 2 },
+  front : {opacity: 1, scale: 1, x: 0, zIndex: 3 },
+  right : {opacity: 1, scale: 1, x: 400, zIndex: 4 }
 }
 
 const Slider = () => {
@@ -18,34 +18,29 @@ const Slider = () => {
   const slides = [1,2,3,4,5,6,7,8,9,10];
   const [frontSlideIndex, setFrontSlideIndex] = useState(1);
 
-  console.log(frontSlideIndex)
-
   const mappedSlides = useMemo(() => {
     return slides.map((data, key) => {
-      let state : slidesState = "front";
-      if (key < frontSlideIndex) state = "left"
-      if (key === frontSlideIndex) state = "front"
-      if (key > frontSlideIndex) state = "right"
-
-      return <motion.div 
+      const state: slidesState = key < frontSlideIndex ? 
+                                "left" : key === frontSlideIndex ? "front" : "right";
+      return (
+      <motion.div 
         key={key} 
         className={styles.slideMotion} 
-        transition={{ type: "spring", stiffness: "50" }}
+        transition={{ type: "spring", typstiffness: "100", bounce: 0.1 }}
+        onAnimationComplete={() => key === frontSlideIndex ? console.log("Play", key) : null}
         animate={state}
         variants={variants}
         >
         <Slide />
       </motion.div>
-    }
-    )
-  },[slides, frontSlideIndex])
+      )})},[slides, frontSlideIndex])
 
   const toggleRight = () => {
-    setFrontSlideIndex((current) => current < slides.length -1 ? current + 1 : slides.length)
+    setFrontSlideIndex((current) => current < slides.length - 1 ? current + 1 : current)
   }
 
   const toggleLeft = () => {
-    setFrontSlideIndex((current) => current > 0 ?  current-1 : 0)
+    setFrontSlideIndex((current) => current > 0 ?  current - 1 : 0)
   }
 
 
@@ -53,7 +48,7 @@ const Slider = () => {
   return (
 		<div className={styles.bigContainer}>
       <div className={styles.sliderContainer}>
-          {mappedSlides}
+        {mappedSlides}
       </div>
       <div className={styles.buttonContainer}>
         <button onClick={toggleRight}>right</button>
