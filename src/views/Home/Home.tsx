47bfb2db0cf4778c45358ee9ae11b3d38ac282ea
PawelDuplaga <pawelduplagait@gmail.com>
2023-11-05 17:13:57 +0100

    fixed build

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -3,21 +3,23 @@ import { TSlide } from '@/lib/types/Slide';
 import SlideContextProvider from '@/context/slide-context';
 import SongSnippetSlider from '@/components/SongSnippetSlider';
 import axiosClient from "@/utils/axiosClient";
+import ErrorPage from '@/components/ErrorPage';
 
 const getSlides = async () => {
-  
   try {
     const backendResponse = await axiosClient.get('/slides');
     const slides: TSlide[] = backendResponse.data;
     return slides
   } catch (error) {
-    throw new Error("Something went wrong!")
+    console.error("Cant collect the data from the server");
   }
 }
 
 const Home = async () => {
 
   const slides = await getSlides();
+
+  if (!slides) return <ErrorPage />
 
   return (
     <SlideContextProvider slides={slides}>
