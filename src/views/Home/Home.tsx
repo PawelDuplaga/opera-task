a8de86260c33eb834c9978f4283811cce15a60f3
PawelDuplaga <pawelduplagait@gmail.com>
2024-02-12 12:57:58 +0100

    added serverless functionality for presentation purpose

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,19 +1,11 @@
-'use client'
+
 
 import styles from './styles.module.scss';
 import { TSlide } from '@/lib/types/Slide';
 import { AudioSnippetData } from '@/lib/const/audioSnippetData';
 import SlideContextProvider from '@/context/slide-context';
 import SongSnippetSlider from '@/components/SongSnippetSlider';
-import { useEffect, useState } from 'react';
-import ErrorPage from '@/components/ErrorPage';
-import Spin from '@/components/Spin';
-import axios from 'axios';
 
-const Home = () => {
-
-  const [slides, setSlides] = useState<TSlide[]>();
-  const [error, setError] = useState(null);
 
 const getSlidesLocal = (): TSlide[] => {
   return AudioSnippetData.map((elem, index): TSlide => ({
@@ -28,35 +20,16 @@ const getSlidesLocal = (): TSlide[] => {
 
 const Home = async () => {
 
-  // const slides = await getSlides();
   const slides = getSlidesLocal();
-  useEffect(() => {
-    axios
-      .get('/api/slides') // proxy in next.js.config
-      .then(response => {
-        setSlides(response.data);
-      })
-      .catch(error => {
-        setError(error);
-      });
-  },[])
-
 
   return (
     <div className={styles.homeContainer}>
-    {error ? (
-      <ErrorPage />
-    ) : slides ? (
-      <SlideContextProvider slides={slides}>
-          <SongSnippetSlider />
-      </SlideContextProvider >
-    ) : (
-      <div className={styles.loading}>
-        <Spin />
-      </div>
-    )}
-  </div>
+        <SlideContextProvider slides={slides}>
+            <SongSnippetSlider />
+        </SlideContextProvider >
+    </div>
   );
 };
+
 
 export default Home
