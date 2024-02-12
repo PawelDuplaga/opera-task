6ed1fa102c79d90aaf06a8b65f3844c4d9e1886f
PawelDuplaga <pawelduplagait@gmail.com>
2024-02-12 12:50:03 +0100

    added serverless functionality for presentation purpose

---
+++ src/lib/const/audioSnippetData.ts
@@ -0,0 +1,66 @@
+export const AudioSnippetData = [
+    {
+        "id": "1",
+        "artist" : "A$AP Rocky",
+        "title" : "Praise the Lord (Da Shine)",
+        "album" : "Testing",
+        "imageName" : "Asap_Rocky_Praise_The_Lord.jpeg",
+        "audioName" : "A$AP_Rocky_Praise_The_Lord.mp3"
+    },
+    {
+        "id": "2",
+        "artist" : "Kendrick Lamar",
+        "title" : "All The Stars",
+        "album" : "Black Panther: The Album",
+        "imageName": "Kendrick_stars.jpeg",
+        "audioName": "Kendrick_Lamar_All_the_Stars.mp3"
+    },
+    {
+        "id": "3",
+        "artist" : "Jungle",
+        "title" : "Back On 74",
+        "album" : "Volcano",
+        "imageName": "Jungle_74.jpg",
+        "audioName": "Jungle_Back_On_74.mp3"
+    },
+    {
+        "id": "4",
+        "artist" : "Skepta",
+        "title" : "Shutdown",
+        "album" : "Konnichiwa",
+        "imageName": "Skepta_Shutdown.jpg",
+        "audioName": "Skepta_Shutdown.mp3"
+    },
+    {
+        "id": "5",
+        "artist" : "Skepta",
+        "title" : "That's Not Me",
+        "album" : "Konnichiwa",
+        "imageName": "Skepta_Thats_Not_Me.jpg",
+        "audioName": "Skepta_Thats_not_me.mp3"
+    },
+    {
+        "id": "6",
+        "artist" : "Tame Impala",
+        "title" : "The Less I Know the Better",
+        "album" : "Currents",
+        "imageName": "Tale_Impala_The_Less_I_Know.jpg",
+        "audioName": "Tame_Impala_The_Less_I_Know_The_Better.mp3"
+    },
+    {
+        "id": "7",
+        "artist" : "Yeat",
+        "title" : "Nun id change",
+        "album" : "AftërLyfe",
+        "imageName": "Yeat_Nun.jpeg",
+        "audioName": "Yeat_Nun.mp3"
+    },
+    {
+        "id": "8",
+        "artist" : "Yeat",
+        "title" : "Poppin",
+        "album" : "2 Alivë",
+        "imageName": "Yeat_Poppin.png",
+        "audioName": "Yeat_Poppin.mp3"
+    }
+] as const;
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,5 +1,6 @@
 import styles from './styles.module.scss';
 import { TSlide } from '@/lib/types/Slide';
+import { AudioSnippetData } from '@/lib/const/audioSnippetData';
 import SlideContextProvider from '@/context/slide-context';
 import SongSnippetSlider from '@/components/SongSnippetSlider';
 import axiosClient from "@/utils/axiosClient";
@@ -15,9 +16,21 @@ const getSlides = async () => {
   }
 }
 
+const getSlidesLocal = (): TSlide[] => {
+  return AudioSnippetData.map((elem, index): TSlide => ({
+    id: elem.id, // Replace with the actual type of elem.id
+    artist: elem.artist, // Replace with the actual property name in elem
+    title: elem.title, // Replace with the actual property name in elem
+    album: elem.album, // Replace with the actual property name in elem
+    image_url: `/images/${elem.imageName}`, // Replace with the actual property name in elem
+    audio_url: `/audio/${elem.audioName}`, // Replace with the actual property name in elem
+  }));
+};
+
 const Home = async () => {
 
-  const slides = await getSlides();
+  // const slides = await getSlides();
+  const slides = getSlidesLocal();
 
   return (
     <SlideContextProvider slides={slides}>
